78643146bb2f005aa83132260af0959434900bf7
unknown <haquemahmudul600@gmail.com>
2024-11-19 21:43:45 +0600

    shawon commit in learning c 2

--- learning.c
+++ learning.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
     char a ;
-    printf("Please enter a single character");
-    scanf("%c" , &a);
-    printf("%c", a);
-    c
+    // printf("Please enter a single character");
+    // scanf("%c" , &a);
+    // printf("%c", a);
+    
     
     
    
